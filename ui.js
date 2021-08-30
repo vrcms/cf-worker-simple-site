e57fc64b14deb4f41e415b9ec38da81e706dba40
Jason.lu <vrcms@users.noreply.github.com>
2021-08-30 12:27:19 +0800

    v1.0.6

--- ui.js
+++ ui.js
@@ -2,28 +2,46 @@ const clearurlbtn = `
 <style>
 #clearsite {    
     position: fixed;
-    right: 5px;
+    right: 2px;
     top:25%;
-	width: 60px;    
+	height:35px;
+	width: 35px;    
 
 
     border:1px solid #4183c4;
-    padding:5px 5px;
+    padding:0;
 
     border-color: #eaeaea;
     background: #fafafa;
     cursor: default;
     
     color: #999;    
+   
 
-	
+	background-color: rgba(214,242,255,0.95);
+    border-radius: 3px;
+    font-size: 0;
+    box-shadow: 0 1px 4px 0 rgb(28 32 42 / 36%);
+    margin-left: auto;
 
-    box-shadow: -1px 0 6px rgb(0 0 0 / 20%);
+
     cursor: pointer;
     z-index:9999;
 }
+
+#clearsite::before {
+    content: '';
+    display: inline-block;
+    width: 35px;
+    height: 35px;
+    background-image: url(https://cdn.jsdelivr.net/gh/vrcms/cf-worker-simple-site@1.0.6/elevator_icons_v4.svg);
+	transform:rotate(-90deg);
+    background-repeat: no-repeat;
+    background-size: auto;
+	background-position: -24px 8px;
+}
 </style>
-<button id="clearsite" title="点击返回 click go back to the CF-WORKER">返回Back</button>
+<button id="clearsite" title="点击返回 click go back to the CF-WORKER">B</button>
 <script>
   var btn = document.getElementById("clearsite");
         
